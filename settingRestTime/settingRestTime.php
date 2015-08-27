05beb3b9fc8740f9726357242a12841c5c5d4a10
suzukikazuhiro <cimkazhiro.szk@gmail.com>
2015-08-27 17:48:28 +0900

    変更内容
    休憩時間設定のDB登録・変更を実装

--- settingRestTime/settingRestTime.php
+++ settingRestTime/settingRestTime.php
@@ -1,11 +1,32 @@
 <?php
+header('Content-type: text/html; charset=UTF-8');
+
+//インクルード
+require_once('../lib/mysql.inc');
+require_once('../lib/db.inc');
+require_once('settingRestTime_model.inc');
+
+//データベース接続
+if(!connectDatabase($db)){
+	$errmsg = "DB接続エラーが発生しました。";
+	//エラー画面へ遷移
+	callErrorPaga($errMsg);
+	exit;
+}
+
+//クラスをインスタンス化
+$model = new settingRestTime_model();
+
+//画面項目取得
+$model->getForm();
+
+//処理
+$model->prosessing($db);
+
+//画面呼び出し
+require_once('settingRestTime.phtml');
+
+//データベース切断
+closeDatabase($db)
 
 ?>
-<html>
-	<head>
-		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	</head>
-    <body>
-
-    </body>
-</html>
