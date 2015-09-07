13dc3b0ff6e9e2cbc57a7c30eebaf68e80732484
suzukikazuhiro <cimkazhiro.szk@gmail.com>
2015-09-07 18:01:17 +0900

    変更内容
    勤務時間表（個人）の作成

--- personalWorkTime/personalWorkTime.php
+++ personalWorkTime/personalWorkTime.php
@@ -19,7 +19,7 @@
 		require_once "../lib/db.inc";				//データベース情報用ファイル
 		require_once "../lib/mysql.inc";			//データベース接続用ファイル
 		require_once '../lib/util.inc';				//一般用クラスファイル
-		require_once "overallWorkTime_model.inc";	//クラスファイル
+		require_once "personalWorkTime_model.inc";	//クラスファイル
 
 		//データベース接続
 		if(!connectDatabase($db)){
@@ -30,7 +30,7 @@
 	}
 
 		//クラスをインスタンス化
-		$model = new overallWorkTime_model();
+		$model = new personalWorkTime_model();
 
 		//画面項目の取得
 		$model->getForm();
