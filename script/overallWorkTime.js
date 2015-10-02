133abc136afa868f1a621e82ff7532ecbbd3df30
suzukikazuhiro <cimkazhiro.szk@gmail.com>
2015-10-02 17:57:19 +0900

    変更内容
    ■勤務時間表（全体,個人）
    
    overallWorkTime.js
    overallWorkTime.phtml
    overallWorkTime_model.inc
    personalWorkTime_model.inc
    workTime.js
    
    ・勤務時間表の表示,form部分の仕様を変更。

--- script/overallWorkTime.js
+++ script/overallWorkTime.js
@@ -1,7 +1,6 @@
 $(function(){
 
 	$('#display').click(function(){
-
 		if($("input[name='work']:checked").val() == "month"){
 
 			if($("input[name='month']").val().length == 0){
@@ -11,16 +10,20 @@ $(function(){
 
 			}
 
-			var date = new Date($("input[name='month']").val());
-			var year = date.getFullYear();
+			if(!$("input[name='month']").val().match(/^\d{4}-[01]\d$/)){
+
+				alert("数字と-以外の文字が入力されています！");
+				return false;
+
+			}
+
+			var d = $("input[name='month']").val().replace( /-/g , "/" ) + "/01";
+			var formDate = new Date(d);
+
+			var date = new Date();
+			var year = date.getFullYear()+1;
 			var month = date.getMonth()+1;
-			var formDate = year + "/" + month + "/1";
-
-			var date2 = new Date();
-			var year2 = date2.getFullYear()+1;
-			var month2 = date2.getMonth()+1;
-			var maxDate = year2 + "/" + month2 + "/1";
-
+			var maxDate = new Date(year,month);
 
 			if(formDate > maxDate){
 
@@ -29,7 +32,8 @@ $(function(){
 
 			}
 
-			var minDate = "2000/1/1"
+			var minDate = new Date("2000/01/01");
+
 			if(formDate < minDate){
 
 				alert("検索する月が2000年以前です！");
@@ -42,15 +46,24 @@ $(function(){
 
 			var errArray = new Array();
 
+
 			if($("input[name='startDay']").val().length == 0){
 
 				errArray[0] = "開始日が入力されていません！";
+
+			}else if(!$("input[name='startDay']").val().match(/^\d{4}-[01]\d$/)){
+
+				errArray[0] = "開始日のフォームに数字と-以外の文字が入力されています！";
 
 			}
 
 			if($("input[name='endDay']").val().length == 0){
 
 				errArray[1] = "終了日が入力されていません！";
+
+			}else if(!$("input[name='endDay']").val().match(/^\d{4}-[01]\d$/)){
+
+				errArray[1] = "終了日のフォームに数字と-以外の文字が入力されています！";
 
 			}
 
@@ -61,9 +74,25 @@ $(function(){
 
 			}
 
-			if($("input[name='endDay']").val() > if($("input[name='startDay']").val()){
 
-				errArray[1] = "終了期間が開始期間よりも前に設定しています！";
+			var startDay = $("input[name='startDay']").val().replace( /-/g , "/" );
+			var endDay = $("input[name='endDay']").val().replace( /-/g , "/" );
+
+
+			if(startDay > endDay){
+
+				alert("終了期間が開始期間よりも前に設定しています！");
+				return false;
+
+			}
+
+			var maxPeriod = new Date(startDay + "/01");
+			maxPeriod.setFullYear(maxPeriod.getFullYear()+1);
+
+			var endPeriod = new Date(endDay + "/01");
+			if(endPeriod >= maxPeriod){
+
+				alert("検索する期間が1年以上です！");
 				return false;
 
 			}
--- script/workTime.js
+++ script/workTime.js
@@ -1,63 +1,5 @@
 $(function(){
 
-	$('#display').click(function(){
-
-		if($("input[name='work']:checked").val() == "month"){
-
-			if($("input[name='month']").val().length == 0){
-
-				alert("月次が入力されていません！");
-				return false;
-
-			}
-
-			var date = $("input[name='month']").val();
-			var date2 = new Date();
-			var year = date2.getFullYear()+1;
-			var month = date2.getMonth()+1;
-			var maxDate = year + "-" + month;
-			if(date > maxDate){
-				alert("検索する月が1年以上先です！");
-				return false;
-
-			}
-
-			var minDate = new Date(2000,0,1)
-			if(date < minDate){
-
-				alert("検索する月が2000年以前です！");
-				return false;
-
-			}
-		}
-
-		if($("input[name='work']:checked").val() == "period"){
-
-			var errArray = new Array();
-
-			if($("input[name='startDay']").val().length == 0){
-
-				errArray[0] = "開始日が入力されていません！";
-
-			}
-
-			if($("input[name='endDay']").val().length == 0){
-
-				errArray[1] = "終了日が入力されていません！";
-
-			}
-
-			if(errArray.length != 0){
-
-				alert(errArray.join("\n"));
-				return false;
-
-			}
-
-		}
-
-	});
-
 	$('a[href="../login/login.php"]').click(function() {
 		if (!confirm('ログアウトします。\nよろしいですか？')) {
 			return false;
@@ -82,34 +24,6 @@ $(function(){
 		}
 	});
 
-	//オンロードさせ、リロード時に選択を保持
-	window.onload = formChange;
-
 });
 
-function formChange(){
-	radio = document.getElementsByName('work')
-	if(radio[0].checked) {
-
-		//フォーム
-		document.getElementById('formYear').style.display = "";
-		document.getElementById('formMonth').style.display = "none";
-		document.getElementById('formPeriod').style.display = "none";
-
-	}else if(radio[1].checked) {
-
-		//フォーム
-		document.getElementById('formYear').style.display = "none";
-		document.getElementById('formMonth').style.display = "";
-		document.getElementById('formPeriod').style.display = "none";
-
-	}else if(radio[2].checked) {
-
-		//フォーム
-		document.getElementById('formYear').style.display = "none";
-		document.getElementById('formMonth').style.display = "none";
-		document.getElementById('formPeriod').style.display = "";
-
-	}
-}
 
