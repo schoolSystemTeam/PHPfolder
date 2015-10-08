794ad6c9994f5f4f1f7b07bc04668bdcb1927acc
suzukikazuhiro <cimkazhiro.szk@gmail.com>
2015-10-08 10:48:49 +0900

    変更内容
    コメントの挿入
    
    勤務時間表（全体）
    ・年次で検索したときメッセージが変化しないのを修正
    ・年次検索後、勤務時間表(個人)に遷移したときに、値が
    設定していないのを修正

--- script/workTime.js
+++ script/workTime.js
@@ -1,24 +1,43 @@
+/*
+ *概要：
+ *	workTime.js
+ *機能説明：
+ *	勤務時間表の各種ボタンとログアウトリンクを
+ *	押下したとき、確認を促すダイアログボックスを表示させる。
+ *備考：
+ *	作成日)	2015/9/03
+ *	作成者)	鈴木一紘
+ *	説明）
+ *	更新日)
+ *	更新者)
+ *	変更)
+ */
+
 $(function(){
 
+	//ログアウトリンクを押下
 	$('a[href="../login/login.php"]').click(function() {
 		if (!confirm('ログアウトします。\nよろしいですか？')) {
 			return false;
 		}
 	});
 
+	//「excelに出力」ボタンを押下
 	$('button.exportExcel').click(function() {
 		if (!confirm('excelに出力します。\nよろしいですか？')) {
 			return false;
 		}
 	});
 
+	//「PDFに出力」ボタンを押下
 	$('button.exportPDF').click(function() {
 		if (!confirm('PDFに出力します。\nよろしいですか？')) {
 			return false;
 		}
 	});
 
-	$('button#record').click(function() {
+	//勤務時間表(個人)画面の「保存」ボタンを押下
+	$('input#record').click(function() {
 		if (!confirm('上記の内容で登録します。\nよろしいですか？')) {
 			return false;
 		}
